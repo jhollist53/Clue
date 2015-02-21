88e39718b835c7c8a9efffc8f76978668a72d58d
Marek Prussak <marek@theprussaks.com>
2015-02-21 15:08:02 -0700

    updated board layout loading

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -6,15 +6,27 @@ import clueGame.RoomCell.DoorDirection;
 
 public class Board {
 	private ArrayList<ArrayList<BoardCell>> boardLayout;
-	private int numRows, numColumns;
-	private Map<Character, String> rooms = new HashMap<Character, String>();
+	private int xDim, yDim;
+	private Map<Character, String> rooms;
 	
-	Board() {
-		
+	public Board() {
+		boardLayout = new ArrayList<ArrayList<BoardCell>>();
+		rooms = new HashMap<Character, String>();
 	}
 	
 	public void loadBoardConfig (String configFile) {
-		Scanner scanner = new Scanner(configFile);
+		readBoardFromFile(configFile);
+		try{
+			verifyBoard();
+		} catch (BadConfigFormatException e) {
+			System.out.println(e);
+		}
+		transposeBoard();
+	}
+	
+	private void readBoardFromFile(String file){
+		//Reads data from file, line by line.
+		Scanner scanner = new Scanner(file);
 		while(scanner.hasNextLine()){
 			//Regular expression, matches any leading whitespace, trailing whitespace, then any other leading whitespace.
 			List<String> temp = Arrays.asList(scanner.nextLine().split("\\s*,\\s*"));
@@ -44,6 +56,28 @@ public class Board {
 				}
 			}
 		}
+		scanner.close();
+	}
+	
+	private void verifyBoard() throws BadConfigFormatException{
+		//Board is not yet transposed, still in form (y,x)
+		xDim = boardLayout.get(0).size();
+		yDim = boardLayout.size();
+		for(ArrayList<BoardCell> x : boardLayout){
+			if (x.size() != xDim){ throw new BadConfigFormatException("Uneven row length detected.");}
+		}
+	}
+	
+	private void transposeBoard(){
+		ArrayList<ArrayList<BoardCell>> temp = new ArrayList<ArrayList<BoardCell>>();
+		for (int i = 0; i < xDim; i++){
+			ArrayList<BoardCell> temp2 = new ArrayList<BoardCell>();
+			for (int j = 0; j < yDim; j ++){
+				temp2.add(boardLayout.get(j).get(i));
+			}
+			temp.add(temp2);
+		}
+		boardLayout = temp;
 	}
 
 	public ArrayList<ArrayList<BoardCell>> getBoardLayout() {
@@ -62,12 +96,12 @@ public class Board {
 		return rooms;
 	}
 
-	public RoomCell getRoomCellAt(int row, int col) {
+	public RoomCell getRoomCellAt(int x, int y) {
 		RoomCell roomCell = new RoomCell();
 		return roomCell;
 	}
 
-	public BoardCell getCellAt(int row, int col) {
+	public BoardCell getCellAt(int x, int y) {
 		BoardCell boardCell = new WalkwayCell();
 		return boardCell;
 	}
